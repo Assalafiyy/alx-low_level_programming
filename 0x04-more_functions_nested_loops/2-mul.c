1e67abfe3fee65f5495d2600be3a5fa6d3af54b9
Abu-sabreen <abusabreen90@gmail.com>
2023-06-02 10:49:22 -0700

    root@604c38731cd0:/alx-low_level_programming/0x04-more_functions_nested_loops# git add . root@604c38731cd0:/alx-low_level_programming/0x04-more_functions_nested_loops# git commit -m

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 /* Multiply the two integers and store the result in a variable*/
 /* Return the result of the multiplication*/
 
-int mul(int a, int b);
+int mul(int a, int b)
 
 {
 	int result = a * b;
