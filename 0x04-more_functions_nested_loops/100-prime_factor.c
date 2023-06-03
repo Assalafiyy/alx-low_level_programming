274fb7485a86b32701828ef192a557a5d9733073
Abu-sabreen <abusabreen90@gmail.com>
2023-06-03 13:30:24 -0700

    print no

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,41 +1,39 @@
 #include <stdio.h>
-#include <math.h>
+#include <main.h>
+
 /**
- * print_triangle - prints a triangle, followed by a new line
- * @size: size of the triangle
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-long long findLargestPrimeFactor(long long num) 
+int main(void)
 {
-	long long largestFactor = -1;
+	long int n;
+	long int max;
+	long int i;
 
-	while (num % 2 == 0) 
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
 	{
-	largestFactor = 2;
-	num /= 2;
+		max = 2;
+		n /= 2;
 	}
 
-	for (long long i = 3; i <= sqrt(num); i += 2) 
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
-	while (num % i == 0) 
-	{
-	largestFactor = i;
-	num /= i;
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
 	}
-	}
 
-	if (num > 2)
-	largestFactor = num;
+	if (n > 2)
+		max = n;
 
-	return largestFactor;
-}
-
-int main(void) 
-{
-	long long num = 612852475143;
-	long long largestPrimeFactor = findLargestPrimeFactor(num);
-
-	printf("%lld\n", largestPrimeFactor);
+	printf("%ld\n", max);
 
 	return (0);
 }
-
