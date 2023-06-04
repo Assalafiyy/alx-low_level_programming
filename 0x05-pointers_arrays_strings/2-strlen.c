89389d20d2316fa17a103f5725550e4cf2e5225b
Abu-sabreen <abusabreen90@gmail.com>
2023-06-04 04:22:45 -0700

    length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,11 +9,26 @@
  */
 int main(void)
 {
-	char *str = "My first strlen!";
-	int len = _strlen(str);
+	int pass[100];
+	int i, sum, n;
 
-	printf("%d\n", len);
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
 	return (0);
 }
-
