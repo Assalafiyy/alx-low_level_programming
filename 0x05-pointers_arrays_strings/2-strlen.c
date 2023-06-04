b06a34ed2c8d513548952d0b82bd651a3937527c
Abu-sabreen <abusabreen90@gmail.com>
2023-06-04 04:20:26 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str = "My first strlen!";
+	int len = _strlen(str);
+
+	printf("%d\n", len);
+
+	return (0);
+}
+
