a7e38f4ccc3b6fe234513534afae2c8751c217de
Abu-sabreen <adamuabdulfatah90@gamil.com>
2023-08-21 10:46:50 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,34 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-#include <time.h>
+
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
- *
- * Return: Always 0 (Success)
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * Return: length
  */
-int main(void)
+int _strlen(char *s)
 {
-	int pass[100];
-	int i, sum, n;
+	int len = 0;
 
-	sum = 0;
-
-	srand(time(NULL));
-
-	for (i = 0; i < 100; i++)
+	while (*s != '\0')
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
+		len++;
+		s++;
 	}
 
-	return (0);
+	return (len);
 }
